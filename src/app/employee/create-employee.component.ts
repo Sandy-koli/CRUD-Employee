a26be8378546f32cf51a7936ab86d4c62c1cc48f
Sandeep Koli <INKOLS02@ingrammgmt.dmz>
2018-03-20 17:55:24 +0530

    Configured bsDatepicker so set min & max date with theme

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../Model/department.model';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 
 @Component({
@@ -10,18 +10,28 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  datepickerConfig: Partial<BsDatepickerConfig>;
+  colorTheme: 'theme-blue';
   // gender = 'female';
   // isActive = true;
   // department = '5';
   departments: Department[] = [
-   { id: 1, name: 'Help Desk'},
-   { id: 2, name: 'HR'},
-   { id: 3, name: 'Admin'},
-   { id: 4, name: 'IT'},
-   { id: 5, name: 'Architect'},
-   { id: 6, name: 'Manager'}
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'Admin' },
+    { id: 4, name: 'IT' },
+    { id: 5, name: 'Architect' },
+    { id: 6, name: 'Manager' }
   ];
-  constructor() { }
+  constructor() {
+    this.datepickerConfig = Object.assign({}, {
+      // containerClass: this.colorTheme,
+      showWeekNumbers: false,
+      minDate: new Date(2018, 0, 1),
+      maxDate: new Date(2018, 12, 31),
+      dateInputFormat: 'DD/MMM/YYYY'
+    });
+  }
 
   ngOnInit() {
   }
